6a215446532ac996bb1621b5bc7d1188b5a5edac
philip howley <howley.phil@gmail.com>
2021-02-22 23:39:16 -0500

    Add single request

--- server.js
+++ server.js
@@ -48,12 +48,26 @@ function filtersByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filtersByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(PORT, () => {
